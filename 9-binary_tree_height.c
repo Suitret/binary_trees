e8c517d89168a037885a30238cb665632effb7e9
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-08-03 13:26:46 +0100

    made changes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,9 +14,9 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL && (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
